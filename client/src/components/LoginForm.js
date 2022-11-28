3b9985e474e453476db70758dbe310ce8ee00333
Leximiller128 <leximiller128@gmail.com>
2022-11-28 10:53:40 -0700

    react bootstrap for loginform

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,89 +1,29 @@
-import React, { useState } from "react";
-import "./style.css";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
-// Here we import a helper function that will check if the email is valid
-import { checkPassword } from "../../utils/helpers";
-
-function Form() {
-  // Create state variables for the fields in the form
-  // We are also setting their initial values to an empty string
-
-  const [userName, setUserName] = useState("");
-  // TODO: Create a password variable and a function "setPassword" using useState
-  const [password, setPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-
-  const handleInputChange = (e) => {
-    // Getting the value and name of the input which triggered the change
-    const { target } = e;
-    const inputType = target.name;
-    const inputValue = target.value;
-
-    // Based on the input type, we set the state of either email, username, and password
-    // TODO: Add an else statement to the end that will set the password to the value of 'inputValue'
-
-    if (inputType === "userName") {
-      setUserName(inputValue);
-    } else if (inputType === "password") {
-      setPassword(inputValue);
-    }
-  };
-
-  const handleFormSubmit = (e) => {
-    // Preventing the default behavior of the form submit (which is to refresh the page)
-    e.preventDefault();
-
-    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
-    if (!validateEmail(userName)) {
-      setErrorMessage("Email or username is invalid");
-      // We want to exit out of this code block if something is wrong so that the user can correct it
-      return;
-      // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
-    }
-    if (!checkPassword(password)) {
-      setErrorMessage(
-        `Choose a more secure password for the account: ${userName}`
-      );
-      return;
-    }
-
-    // If successful, we want to clear out the input after registration.
-    setUserName("");
-    // TODO: Set the password back to an empty string after the user clicks submit
-    setPassword("");
-    alert(`Hello ${userName}`);
-  };
-
+function BasicExample() {
   return (
-    <div>
-      <p>Hello {userName}</p>
-      <form className="form">
-        <input
-          value={userName}
-          name="userName"
-          onChange={handleInputChange}
-          type="text"
-          placeholder="username"
-        />
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
 
-        <input
-          value={password}
-          name="password"
-          onChange={handleInputChange}
-          type="password"
-          placeholder="password"
-        />
-        <button type="button" onClick={handleFormSubmit}>
-          Submit
-        </button>
-      </form>
-      {errorMessage && (
-        <div>
-          <p className="error-text">{errorMessage}</p>
-        </div>
-      )}
-    </div>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
   );
 }
 
-export default Form;
+export default BasicExample;
