a378d17869b51b12cc77bb2cf6d05ce5f8282ed0
Dana Golebiewski <dana.golebiewski@gmail.com>
2022-11-28 18:36:18 -0700

    started resolvers.js files

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,8 +3,42 @@ const { AuthenticationError } = require("apollo-server-express");
 const { signToken } = require("../utils/auth");
 
 
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select(
+          "-__v -password"
+        );
+        return userData;
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const userData = await User.create(args);
+      const token = signToken(userData);
+      return { userData, token };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
 
+      if (!user) {
+        throw new AuthenticationError("No user found");
+      }
+      const correctPw = await user.isCorrectPassword(password);
 
+      if (!correctPW) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
 
+      const token = signToken(user);
+
+      return { token, user };
+    },
+// need to add 'save card', 'remove card', 
+  },
+};
 
 module.exports = resolvers;
