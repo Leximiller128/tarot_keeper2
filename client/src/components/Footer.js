1e1641e89f2d214356ae1a988806595afb1b9cc2
Leximiller128 <leximiller128@gmail.com>
2022-11-28 18:29:04 -0700

    started footer js

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,2 +1,106 @@
 //import footer.css
-import "../styles/footer.css"
+import "../styles/footer.css";
+import React from "react";
+import {
+  MDBFooter,
+  MDBContainer,
+  MDBCol,
+  MDBRow,
+  MDBIcon,
+  MDBBtn,
+} from "mdb-react-ui-kit";
+
+export default function App() {
+  return (
+    <MDBFooter
+      className="text-center text-white"
+      style={{ backgroundColor: "#f1f1f1" }}
+    >
+      <MDBContainer className="pt-4">
+        <section className="mb-4">
+          <MDBBtn
+            rippleColor="dark"
+            color="link"
+            floating
+            size="lg"
+            className="text-dark m-1"
+            href="#!"
+            role="button"
+          >
+            <MDBIcon fab className="fab fa-facebook-f" />
+          </MDBBtn>
+
+          <MDBBtn
+            rippleColor="dark"
+            color="link"
+            floating
+            size="lg"
+            className="text-dark m-1"
+            href="#!"
+            role="button"
+          >
+            <MDBIcon fab className="fa-twitter" />
+          </MDBBtn>
+
+          <MDBBtn
+            rippleColor="dark"
+            color="link"
+            floating
+            size="lg"
+            className="text-dark m-1"
+            href="#!"
+            role="button"
+          >
+            <MDBIcon fab className="fa-google" />
+          </MDBBtn>
+
+          <MDBBtn
+            rippleColor="dark"
+            color="link"
+            floating
+            size="lg"
+            className="text-dark m-1"
+            href="#!"
+            role="button"
+          >
+            <MDBIcon fab className="fa-instagram" />
+          </MDBBtn>
+
+          <MDBBtn
+            rippleColor="dark"
+            color="link"
+            floating
+            size="lg"
+            className="text-dark m-1"
+            href="#!"
+            role="button"
+          >
+            <MDBIcon fab className="fa-linkedin" />
+          </MDBBtn>
+
+          <MDBBtn
+            rippleColor="dark"
+            color="link"
+            floating
+            size="lg"
+            className="text-dark m-1"
+            href="#!"
+            role="button"
+          >
+            <MDBIcon fab className="fa-github" />
+          </MDBBtn>
+        </section>
+      </MDBContainer>
+
+      <div
+        className="text-center text-dark p-3"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+      >
+        © 2022 Copyright:
+        <a className="text-dark" href="https://mdbootstrap.com/">
+          MDBootstrap.com
+        </a>
+      </div>
+    </MDBFooter>
+  );
+}
