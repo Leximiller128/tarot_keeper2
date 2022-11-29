0bdfa801b71ead925cea9d091cf94b607429b3b9
Leximiller128 <leximiller128@gmail.com>
2022-11-28 18:20:13 -0700

    header js started

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,2 +1,52 @@
 //import header.css
-import "../styles/header.css"
+import "../styles/header.css";
+import React, { useState } from "react";
+import {
+  MDBNavbar,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBNavbarToggler,
+  MDBContainer,
+  MDBIcon,
+  MDBBtn,
+  MDBCollapse,
+} from "mdb-react-ui-kit";
+
+export default function App() {
+  const [showBasic, setShowBasic] = useState(false);
+
+  return (
+    <header>
+      <MDBNavbar expand="lg" light bgColor="white" fixed>
+        <MDBContainer fluid>
+          <MDBNavbarToggler
+            aria-controls="navbarExample01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <MDBIcon fas icon="bars" />
+          </MDBNavbarToggler>
+          <MDBCollapse show={showBasic}>
+            <MDBNavbarNav right className="mb-2 mb-lg-0">
+              <MDBNavbarItem active>
+                <MDBNavbarLink aria-current="page" href="#">
+                  Start New Reading
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink href="#">Past Readings</MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink href="#">My Friend's Readings</MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink href="#">Library Search</MDBNavbarLink>
+              </MDBNavbarItem>
+            </MDBNavbarNav>
+          </MDBCollapse>
+        </MDBContainer>
+      </MDBNavbar>
+    </header>
+  );
+}
