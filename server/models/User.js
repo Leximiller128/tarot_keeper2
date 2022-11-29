c0fdec618dd7dda3156fcb7b4c1d15a09a3e80ae
Dana Golebiewski <dana.golebiewski@gmail.com>
2022-11-28 18:28:33 -0700

    started user.js model

--- server/models/User.js
+++ server/models/User.js
@@ -1,7 +1,49 @@
 const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
 
-//new schema goes in here 
+//might have to import tarot schema? Will there be tarot schema?
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must use a valid email address'],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    //  We will use this if there is a tarot schema 
+    // savedBooks: [bookSchema],
+  },
+  // set this to use virtual below
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+// hash user password
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// custom method to compare and validate password for logging in
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 const User = model('User', userSchema);
 
