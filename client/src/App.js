b300c537c892e2347da4f73affcad269be28b65c
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-11-30 10:55:22 -0700

    fiddling around in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
 
 import Footer from "./components/Footer";
-import Header from "./components/Header";
 import Home from "./components/Home";
 // import Library from "./components/Library";
 // import LoginForm from "./components/LoginForm";
@@ -24,7 +23,7 @@ const App = () => {
         <div className="bodydiv">
           <Routes>
             <Route path="/" element={<Home />}></Route>
-            <Route path="/Navigation" element={<Header />}></Route>
+            <Route path="/Navigation" element={<Navigation />}></Route>
             {/* <Route path="/NewReading" element={<NewReading />}></Route> */}
             <Route path="/Footer" element={<Footer />}></Route>
             {/* <Route path="/" element={<NewReading />}></Route>
@@ -32,7 +31,6 @@ const App = () => {
           <Route path="/SignupForm" element={<SignupForm />}></Route>
           <Route path="/Library" element={<Library />}></Route>
           <Route path="/LoginForm" element={<LoginForm />}></Route>
-          <Route path="/Navigation" element={<Navigation />}></Route>
           <Route path="/PastReadings" element={<PastReadings />}></Route>
           <Route path="/TextArea" element={<TextArea />}></Route>
           <Route path="/SearchForm" element={<SearchForm />}></Route> */}
