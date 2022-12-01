3685c81779a8f711c8330a926bf57b36d8251b25
Leximiller128 <leximiller128@gmail.com>
2022-12-01 10:49:08 -0700

    added text area to new reading

--- client/src/components/NewReading/index.js
+++ client/src/components/NewReading/index.js
@@ -1,14 +1,32 @@
-// import newreading.css
 import "./newreading.css";
+
 import React from "react";
-// import Footer from "./Footer";
+import {
+  MDBContainer,
+  MDBBtn,
+  MDBInputGroup,
+  MDBTextArea,
+  MDBInput,
+} from "mdb-react-ui-kit";
 
 export default function NewReading() {
   return (
-    <>
-          {/* <Header /> */}
-          <h1>Hello</h1>
-    </>
+    <MDBContainer>
+      <MDBContainer fluid>
+        <MDBInputGroup tag="form" className="d-flex w-auto mb-3">
+          <input
+            className="form-control"
+            placeholder="Type query"
+            aria-label="Search"
+            type="Search"
+          />
+          <MDBBtn outline>Search</MDBBtn>
+        </MDBInputGroup>
+      </MDBContainer>
+      <MDBInput label="Title for New Reading" id="typeText" type="text" />
+      <MDBInput label="Inital Question Asked" id="typeText" type="text" />
+      <MDBTextArea label="Notes" id="textAreaExample" rows={4} />
+      <MDBBtn>Submit</MDBBtn>
+    </MDBContainer>
   );
 }
-
