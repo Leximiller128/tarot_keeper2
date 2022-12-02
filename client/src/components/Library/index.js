5e3d8fa0efe6cd0a1c7faa2275d76267daec8c90
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-12-01 20:36:54 -0700

    hover css to library button

--- client/src/components/Library/index.js
+++ client/src/components/Library/index.js
@@ -25,7 +25,7 @@ export default function Library() {
             aria-label="Search"
             type="Search"
           />
-          <MDBBtn className="searchBtn" outline>
+          <MDBBtn className="searchBtn">
             Search
           </MDBBtn>
         </MDBInputGroup>
