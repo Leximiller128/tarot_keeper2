4951232e39f56d1eec270270eee12a24b9f7d0d9
Leximiller128 <leximiller128@gmail.com>
2022-12-04 15:18:58 -0700

    changed home page

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import {
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
-} from '@apollo/client';
+} from "@apollo/client";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import Footer from "./components/Footer";
@@ -19,23 +19,22 @@ import TextArea from "./components/TextArea";
 import SearchForm from "./components/SearchForm";
 import FriendReadings from "./components/FriendReadings";
 import Login from "./components/Login";
-import { setContext } from '@apollo/client/link/context';
-
+import { setContext } from "@apollo/client/link/context";
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('id_token');
+  const token = localStorage.getItem("id_token");
   // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
@@ -54,7 +53,7 @@ function App() {
           <Navigation />
           <div className="bodydiv">
             <Routes>
-              <Route path="/" element={<Home />}></Route>
+              <Route path="/" element={<Login />}></Route>
               <Route path="/Navigation" element={<Navigation />}></Route>
               <Route path="/NewReading" element={<NewReading />}></Route>
               <Route path="/Footer" element={<Footer />}></Route>
