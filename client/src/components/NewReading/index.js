9ce2843c0572dde31737bea7afeebe2f54722989
Dana Golebiewski <dana.golebiewski@gmail.com>
2022-12-05 19:36:19 -0700

    added createreading and searchcard mutation

--- client/src/components/NewReading/index.js
+++ client/src/components/NewReading/index.js
@@ -1,4 +1,5 @@
 import "./newreading.css";
+// import { Card } from "../../../../server/models/tarotCard";
 
 import React from "react";
 import {
@@ -14,6 +15,78 @@ import {
   MDBCardImage,
 } from "mdb-react-ui-kit";
 
+// const SearchCard = () => {
+//   // create state for holding returned google api data
+//   const [searchedCards, setSearchedCards] = useState([]);
+//   // create state for holding our search field data
+//   const [searchInput, setSearchInput] = useState('');
+
+//   // create state to hold saved bookId values
+//   const [savedCardIds, setSavedCardIds] = useState(getSavedCardIds());
+
+//   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
+//   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
+//   useEffect(() => {
+//     return () => saveCardIds(savedCardIds);
+//   });
+
+//   // create method to search for books and set state on form submit
+//   const handleFormSubmit = async (event) => {
+//     event.preventDefault();
+
+//     if (!searchInput) {
+//       return false;
+//     }
+
+//     try {
+//       const response = await searchGoogleBooks(searchInput);
+
+//       if (!response.ok) {
+//         throw new Error('something went wrong!');
+//       }
+
+//       const { items } = await response.json();
+
+//       const cardData = items.map((card) => ({
+//         bookId: book.id,
+//         authors: book.volumeInfo.authors || ['No author to display'],
+//         title: book.volumeInfo.title,
+//         description: book.volumeInfo.description,
+//         image: book.volumeInfo.imageLinks?.thumbnail || '',
+//       }));
+
+//       setSearchedBooks(bookData);
+//       setSearchInput('');
+//     } catch (err) {
+//       console.error(err);
+//     }
+//   };
+
+//   // create function to handle saving a book to our database
+//   const handleSaveBook = async (bookId) => {
+//     // find the book in `searchedBooks` state by the matching id
+//     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
+
+//     // get token
+//     const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+//     if (!token) {
+//       return false;
+//     }
+
+//     try {
+//       const response = await saveBook(bookToSave, token);
+
+//       if (!response.ok) {
+//         throw new Error('something went wrong!');
+//       }
+
+//       // if book successfully saves to user's account, save book id to state
+//       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
+//     } catch (err) {
+//       console.error(err);
+//     }
+//   };
 export default function NewReading() {
   return (
     <>
@@ -24,20 +97,22 @@ export default function NewReading() {
         aria-hidden="true"
       >
         <MDBCardImage
-          src="https://mdbcdn.b-cdn.net/img/new/standard/nature/182.webp"
+          path={Card.img}
           position="top"
           alt="Sunset Over the Sea"
         />
         <MDBCardBody>
           <MDBCardTitle className="placeholder-glow">
-            <span className="placeholder col-6"></span>
+            <span className="placeholder col-6">{Card.name}</span>
           </MDBCardTitle>
           <MDBCardText className="placeholder-glow">
-            <span className="placeholder col-7"></span>
-            <span className="placeholder col-4"></span>
-            <span className="placeholder col-4"></span>
-            <span className="placeholder col-6"></span>
-            <span className="placeholder col-8"></span>
+            <span className="placeholder col-7">{Card.arcana}</span>
+            <span className="placeholder col-4">{Card.suit}</span>
+            <span className="placeholder col-4">{Card.fortune_telling}</span>
+            <span className="placeholder col-6">{Card.keywords}</span>
+            <span className="placeholder col-8">{Card.meaning_up}</span>
+            <span className="placeholder col-8">{Card.meaning_rev}</span>
+            <span className="placeholder col-8">{Card.desc}</span>
           </MDBCardText>
           <MDBBtn id="search-btn">Add Card</MDBBtn>
         </MDBCardBody>
@@ -63,4 +138,5 @@ export default function NewReading() {
       </MDBContainer>
     </>
   );
-}
+};
+// }
