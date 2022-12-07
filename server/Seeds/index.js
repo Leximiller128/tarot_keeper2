553b0318d06981a7701049aee27b7da528c47cd1
Lorena Morales <Lorenarm.999@gmail.com>
2022-12-06 22:02:10 -0700

    userSeed

--- server/Seeds/index.js
+++ server/Seeds/index.js
@@ -1,12 +1,14 @@
 const db = require('../config/connection');
-const { Card } = require('../models');
+const { Card, User } = require('../models');
 
 const cardData = require('./tarot-images.json');
+const userData = require('./user.json')
 
 db.once('open', async () => {
   await Card.deleteMany({});
 
   await Card.insertMany(cardData);
+  await User.insertMany(userData)
 
   console.log('cards seeded!');
   process.exit(0);
